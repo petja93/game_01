f917a8fb0c9b5139768f066586da1d3506677a1c
petja93 <peter.sokolov@haw-hamburg.de>
2017-03-27 16:23:34 +0200

    Code überarbeitet

---
+++ UnityGame01/Assets/Scripts/Cylinder001Eingang.js
@@ -0,0 +1,31 @@
+﻿var ZylinderDeckel2 : GameObject;
+var StoodOn : int;
+
+var qwertz : int;
+
+
+function OnTriggerEnter (col : Collider){
+	StoodOn = 1;
+}
+
+function OnTriggerExit (col : Collider){
+	StoodOn = 0;
+}
+
+function Update(){
+	if(Input.GetButtonDown("GoDown")){
+		if(StoodOn == 1){
+			//GameObject.find("FPSController").GetComponent("FirstPersonController").enabled=false;
+			transform.position = Vector3(0, -1000, 0);
+			WaitingforPipe();
+			qwertz = 1;
+		}
+	}
+}
+
+function WaitingforPipe(){
+	ZylinderDeckel2.GetComponent("Animator").enabled = true;
+	yield WaitForSeconds(2);
+	ZylinderDeckel2.GetComponent("Animator").enabled = false;
+	//GameObject.find("FPSController").GetComponent("FirstPersonController").enabled=true;
+}
