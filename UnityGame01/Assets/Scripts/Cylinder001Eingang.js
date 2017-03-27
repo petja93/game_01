69fd2fa04887a5e359ec0d59e6bd676781e0542a
petja93 <peter.sokolov@haw-hamburg.de>
2017-03-27 21:54:33 +0200

    Wände hinzugefügt, Script angepasst

--- UnityGame01/Assets/Scripts/Cylinder001Eingang.js
+++ UnityGame01/Assets/Scripts/Cylinder001Eingang.js
@@ -1,6 +1,10 @@
 ﻿var ZylinderDeckel2 : GameObject;
 var StoodOn : int;
 
+var MainCam : GameObject;
+var SecondCam: GameObject;
+var MainPlayer : GameObject;
+
 
 function OnTriggerEnter (col : Collider){
 	StoodOn = 1;
@@ -24,5 +28,8 @@ function WaitingforPipe(){
 	ZylinderDeckel2.GetComponent("Animator").enabled = true;
 	yield WaitForSeconds(2);
 	ZylinderDeckel2.GetComponent("Animator").enabled = false;
+	SecondCam.setActive(true);
+	MainCam.setActive(false);
+	MainPlayer.transform.position = Vector3(14.49, -19.13, 0.5);
 	//GameObject.find("FPSController").GetComponent("FirstPersonController").enabled=true;
 }
