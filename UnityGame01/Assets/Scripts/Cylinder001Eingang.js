a9be654e74183293929cebd9ea8ea586af71d72b
petja93 <peter.sokolov@haw-hamburg.de>
2017-03-29 21:32:43 +0200

    Sound hinzugefügt

--- UnityGame01/Assets/Scripts/Cylinder001Eingang.js
+++ UnityGame01/Assets/Scripts/Cylinder001Eingang.js
@@ -5,6 +5,10 @@ var MainCam : GameObject;
 var SecondCam : GameObject;
 var MainPlayer : GameObject;
 
+var FadeScreen : GameObject;
+
+var ZylinderSound : AudioSource;
+
 
 function OnTriggerEnter (col : Collider){
 	StoodOn = 1;
@@ -25,12 +29,20 @@ function Update(){
 }
 
 function WaitingforPipe(){
+	ZylinderSound.Play();
+	FadeScreen.SetActive(true);
 	ZylinderDeckel2.GetComponent("Animator").enabled = true;
-	yield WaitForSeconds(2);
+	yield WaitForSeconds(1.5);
+	FadeScreen.GetComponent("Animator").enabled = true;
+	yield WaitForSeconds(0.495);
+	FadeScreen.GetComponent("Animator").enabled = false;
 	ZylinderDeckel2.GetComponent("Animator").enabled = false;
 	SecondCam.SetActive(true);
 	MainCam.SetActive(false);
 	MainPlayer.transform.position = Vector3(14, -19, 0.5);
-	//MainPlayer.transform.position = Vector3(11, -17, 0.5);
+	FadeScreen.GetComponent("Animator").enabled = true;
+	yield WaitForSeconds(0.495);
+	FadeScreen.GetComponent("Animator").enabled = false;
+	FadeScreen.SetActive(false);
 	//GameObject.find("FPSController").GetComponent("FirstPersonController").enabled=true;
 }
