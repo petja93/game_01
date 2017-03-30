e6688ad355b064b7d7b24786af0350097ab5202d
petja93 <peter.sokolov@haw-hamburg.de>
2017-03-30 18:33:07 +0200

    Pilz und Bewegung hinzugefügt

---
+++ UnityGame01/Assets/Scripts/PilzBewegen.js
@@ -0,0 +1,23 @@
+﻿var leftPoint : float = 13;
+var rightPoint : float = 28;
+var direction : int = 1;
+
+function Update(){
+	if(direction == 1){
+		transform.Translate(Vector3.right * 2 * Time.deltaTime, Space.World);
+		direction = 1;
+	}
+
+	if(this.transform.position.x > rightPoint){
+		direction = 2;
+	}
+
+	if(direction == 2){
+		transform.Translate(Vector3.right * -2 * Time.deltaTime, Space.World);
+		direction = 2;
+	}
+
+	if(this.transform.position.x < leftPoint){
+		direction = 1;
+	}
+}
