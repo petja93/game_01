865af70c7df6711f1389445d7b2d22a3973d9740
petja93 <peter.sokolov@haw-hamburg.de>
2017-03-30 19:18:15 +0200

    Pilz zerstören hinzugefügt

---
+++ UnityGame01/Assets/Scripts/PilzZerstoeren.js
@@ -0,0 +1,10 @@
+﻿var Pilz : GameObject;
+
+function OnTriggerEnter(col : Collider){
+	this.GetComponent("BoxCollider").enabled = false;
+	Pilz.GetComponent("PilzBewegen").enabled = false;
+	Pilz.trasnform.localScale -= new Vector3(0, 0, 0.8);
+	Pilz.transform.localPosition -= new Vector3(0, 0, 0.4);
+	yield WaitForSeconds (1);
+	Pilz.transform.position = Vector3(0, -1000, 0);
+}
