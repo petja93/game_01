15e9900de2fb5a870fec273f5dc8554f66e6389e
petja93 <peter.sokolov@haw-hamburg.de>
2017-03-31 15:12:03 +0200

    Details angepasst

--- UnityGame01/Assets/Scripts/PilzZerstoeren.js
+++ UnityGame01/Assets/Scripts/PilzZerstoeren.js
@@ -3,8 +3,8 @@
 function OnTriggerEnter(col : Collider){
 	this.GetComponent("BoxCollider").enabled = false;
 	Pilz.GetComponent("PilzBewegen").enabled = false;
-	Pilz.trasnform.localScale -= new Vector3(0, 0, 0.8);
+	Pilz.transform.localScale -= new Vector3(0, 0, 0.8);
 	Pilz.transform.localPosition -= new Vector3(0, 0, 0.4);
-	yield WaitForSeconds (1);
+	yield WaitForSeconds(1);
 	Pilz.transform.position = Vector3(0, -1000, 0);
 }
