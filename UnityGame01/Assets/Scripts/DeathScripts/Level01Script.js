0cbe911f72818904eef7c766ebd16b778b251b34
petja93 <peter.sokolov@haw-hamburg.de>
2017-04-06 15:23:06 +0200

    Schattierungen hinzugefügt

--- UnityGame01/Assets/Scripts/DeathScripts/Level01Script.js
+++ UnityGame01/Assets/Scripts/DeathScripts/Level01Script.js
@@ -10,6 +10,8 @@ function OnTriggerEnter(col : Collider){
 	CamFollow.GetComponent("CameraFallout").enabled = false;
 	ThePlayer.GetComponent("ThirdPersonUserControl").enabled = false;
 	ThePlayer.GetComponent("ThirdPersonCharakter").enabled = false;
+	//ThePlayer.transform.localScale -= new Vector3(0.0, 0.7, 0.0);
+	ThePlayer.GetComponent("CapsuleCollider").enabled = false;
 	yield WaitForSeconds(3);
 	Application.LoadLevel(0);
 
